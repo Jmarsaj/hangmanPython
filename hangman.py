3eeef9c59d3da00434eb742674ee90c918061bbd
jmarsaj <jerinsajimon@gmail.com>
2021-05-26 18:39:03 -0700

    Added an else statement that checks if the letter is in the hidden word. It then lets the user know and prints out the hidden word with the found word shown.

--- hangman.py
+++ hangman.py
@@ -12,12 +12,23 @@ def start_round(loaded_word):
     tries = 5
 
     while not guessed and tries > 0:
-        guess = raw_input("Guess a letter or word: ")       # might have to change raw_input to input
+        guess = raw_input("Guess a letter or word: ").upper()       # might have to change raw_input to input
 
         if guess.isalpha() and len(guess) == 1:         # checking if guess is an alphabet
             if guess not in loaded_word:
                 print(guess + " is not in the word")
                 tries -= 1
+            else:
+                print(guess + "is in the word")
+
+                word_as_list = list(hidden_word)
+                indices = [i for i, letter in enumerate(loaded_word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+
+                hidden_word = "".join(word_as_list)
+
+        print(hidden_word)
 
 loaded_word = load_word()
 start_round(loaded_word)
